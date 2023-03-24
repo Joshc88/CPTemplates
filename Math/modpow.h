56399ebde6e630c80e02c0303b7ba7ab83541f02
Joshc88 <joshualiern@gmail.com>
2023-03-24 22:27:20 +0800

    Remove #define ll long long

--- Math/modpow.h
+++ Math/modpow.h
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
 const int MOD = 1000000007;
  
-ll power(int base, int exp = MOD-2) {
+long long power(int base, int exp = MOD-2) {
     if (!exp) return 1;
-    ll res = power(base, exp>>1);
+    long long res = power(base, exp>>1);
     res = (res*res)%MOD;
     if (exp&1) res = (res*base)%MOD;
     return res;
