285291e6cfa9aea4e2e5cfe415ac8b686ad06c88
Joshc88 <joshualiern@gmail.com>
2024-02-19 10:05:03 +0000

    Speed up hashing template

--- String/hashing.h
+++ String/hashing.h
@@ -4,47 +4,53 @@ using namespace std;
 template<typename T, bool R = true, bool LEFT_HASH = true>
 struct hashes {
     mt19937 rng;
+    const static int MOD = 1e9 + 7;
     static int global_base1, global_base2;
-    int base, inv, mod, sz;
-    vector<long long> powers, inv_powers, psa;
+    static vector<long long> powers1, inv_powers1, powers2, inv_powers2;
+    vector<long long> psa;
 
-    hashes(T& s, int m = 1e9 + 7, int b = 131) : rng(chrono::steady_clock::now().time_since_epoch().count()),
-                                                 base(R ? uniform_int_distribution<int>(200, 1000000000)(rng) : b),
-                                                 inv(1), mod(m), sz(s.size()), powers{1}, inv_powers{1}, psa{0} {
+    hashes(T& s, int b = 131) : rng(chrono::steady_clock::now().time_since_epoch().count()), psa{0} {
         int& chosen_base = LEFT_HASH ? global_base1 : global_base2;
-        if (chosen_base) base = chosen_base;
-        else chosen_base = base;
-        long long cur = base;
-        for (int exp = m - 2; exp; exp >>= 1) {
-            if (exp & 1) inv = inv * cur % m;
-            cur = cur * cur % m;
+        vector<long long> &powers = LEFT_HASH ? powers1 : powers2,
+            &inv_powers = LEFT_HASH ? inv_powers1 : inv_powers2;
+        if (!chosen_base) {
+            chosen_base = R ? uniform_int_distribution<int>(200, MOD - 1)(rng) : b;
+            long long cur = chosen_base, inv = 1;
+            for (int exp = MOD - 2; exp; exp >>= 1) {
+                if (exp & 1) inv = inv * cur % MOD;
+                cur = cur * cur % MOD;
+            }
+            powers.push_back(chosen_base);
+            inv_powers.push_back(inv);
         }
-        for (int i = 0; i < s.size(); i++) {
-            powers.push_back(powers.back() * base % mod);
-            inv_powers.push_back(inv_powers.back() * inv % mod);
-            psa.push_back((psa.back() + s[i] * powers[i]) % mod);
+        while (powers.size() < s.size()) {
+            powers.push_back(powers.back() * chosen_base % MOD);
+            inv_powers.push_back(inv_powers.back() * inv_powers[1] % MOD);
         }
+        for (int i = 0; i < s.size(); i++) psa.push_back((psa.back() + s[i] * (LEFT_HASH ? powers1 : powers2)[i]) % MOD);
     }
 
     hashes() {}
 
     long long get(int l, int r) {
-        return (psa[r + 1] - psa[l] + mod) * inv_powers[l] % mod;
+        return (psa[r + 1] - psa[l] + MOD) * (LEFT_HASH ? inv_powers1 : inv_powers2)[l] % MOD;
     }
 };
 
-template<typename T, bool R, bool LEFT_HASH>
-int hashes<T, R, LEFT_HASH>::global_base1 = 0;
-template<typename T, bool R, bool LEFT_HASH>
-int hashes<T, R, LEFT_HASH>::global_base2 = 0;
+template<typename T, bool R, bool LEFT_HASH> int hashes<T, R, LEFT_HASH>::global_base1 = 0;
+template<typename T, bool R, bool LEFT_HASH> int hashes<T, R, LEFT_HASH>::global_base2 = 0;
+template<typename T, bool R, bool LEFT_HASH> vector<long long> hashes<T, R, LEFT_HASH>::powers1 = {1};
+template<typename T, bool R, bool LEFT_HASH> vector<long long> hashes<T, R, LEFT_HASH>::powers2 = {1};
+template<typename T, bool R, bool LEFT_HASH> vector<long long> hashes<T, R, LEFT_HASH>::inv_powers1 = {1};
+template<typename T, bool R, bool LEFT_HASH> vector<long long> hashes<T, R, LEFT_HASH>::inv_powers2 = {1};
 
-template<typename T, bool R = true>
+template<typename T, bool R = true> 
 struct double_hash {
     hashes<T, R, true> hash1;
     hashes<T, R, false> hash2;
 
-    double_hash(T& s, int base1 = 131, int base2 = 173, int mod = 1e9 + 7) : hash1(hashes<T, R, true>(s, mod, base1)),
-                                                                             hash2(hashes<T, R, false>(s, mod, base2)) {}
+    double_hash(T& s, int base1 = 131, int base2 = 173) : hash1(hashes<T, R, true>(s, base1)),
+                                                                             hash2(hashes<T, R, false>(s, base2)) {}
     double_hash() {}
 
     long long get(int l, int r) {
